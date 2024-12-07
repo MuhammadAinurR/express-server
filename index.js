502fb7d7b1dd6c72c7dd4f70b99ca59388372452
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-12-07 19:29:29 +0700

    feat: add logging

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 app.get("/", (req, res) => res.send("Server is up and running"));
 app.use("/api", routes);
 
